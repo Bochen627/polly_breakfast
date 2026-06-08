ccf03d1f35e0b34365a671f879ddd67427ae8e62
Bochen627 <bochenliu627@gmail.com>
2026-06-09 00:14:00 +0800

    ver 4.5

--- replace_step.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('docs/admin.html', 'utf8');
-content = content.replace(/step="0\.01"/g, 'step="any"');
-fs.writeFileSync('docs/admin.html', content);
